d1ff62f8b7c7b2588deb31cb02370d03e53f575b
Dan Roundhill <danroundhill@gmail.com>
2011-09-21 08:07:18 +0000

    Updated version number
    git-svn-id: https://android.svn.wordpress.org/branches/reader@318 a6565c4b-a94d-4d28-bb26-aa9e5ab542c0

--- src/org/wordpress/android/viewStats.java
+++ src/org/wordpress/android/viewStats.java
@@ -953,7 +953,7 @@ public class viewStats extends Activity {
 
 		postMethod.addHeader("charset", "UTF-8");
 		// UPDATE THE VERSION NUMBER BEFORE RELEASE! <3 Dan
-		postMethod.addHeader("User-Agent", "wp-android/1.5");
+		postMethod.addHeader("User-Agent", "wp-android/1.5.1");
 
 		httpParams = postMethod.getParams();
 		HttpProtocolParams.setUseExpectContinue(httpParams, false);
